2ad2791f4176dc48fd910bf045597d6ef7e65020
J. Kim <jeongseok.kim@sk.com>
2020-06-06 00:40:41 +0900

    common: copied common gsettings_new partially from hwangsae

---
+++ gaeul/gaeul.c
@@ -0,0 +1,33 @@
+/**
+ *  Copyright 2019-2020 SK Telecom Co., Ltd.
+ *    Author: Walter Lozano <walter.lozano@collabora.com>
+ *            Jeongseok Kim <jeongseok.kim@sk.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+#include "gaeul.h"
+
+#define G_SETTINGS_ENABLE_BACKEND
+#include <gio/gsettingsbackend.h>
+
+GSettings *
+gaeul_gsettings_new (const gchar * schema_id)
+{
+  g_autoptr (GSettingsBackend) backend = NULL;
+
+  backend = g_keyfile_settings_backend_new ("/etc/hwangsaeul.conf", "/", NULL);
+
+  return g_settings_new_with_backend (schema_id, backend);
+}
---
+++ gaeul/gaeul.h
@@ -0,0 +1,31 @@
+/**
+ *  Copyright 2019-2020 SK Telecom Co., Ltd.
+ *    Author: Walter Lozano <walter.lozano@collabora.com>
+ *            Jeongseok Kim <jeongseok.kim@sk.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+#ifndef __GAEUL_H__
+#define __GAEUL_H__
+
+#include <gio/gio.h>
+
+G_BEGIN_DECLS
+
+GSettings      *gaeul_gsettings_new         (const gchar * schema_id);
+
+G_END_DECLS
+
+#endif // __GAEUL_H__
